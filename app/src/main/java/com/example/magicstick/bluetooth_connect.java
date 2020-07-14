7c9c12230af880d5b54c952ab268587786476bad
seungjun <mickymicmouse@gmail.com>
2020-07-14 14:35:09 +0900

    bluetooth

---
+++ app/src/main/java/com/example/magicstick/bluetooth_connect.java
@@ -0,0 +1,14 @@
+package com.example.magicstick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bluetooth_connect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth_connect);
+    }
+}
